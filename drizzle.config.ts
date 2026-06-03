fb1be75fb09cc5c59d64682ba8a2e9d82e13589e
Shidqi Faadhil Aflah <faadhilshidqi@gmail.com>
2026-06-03 18:56:06 +0700

    fix: perbaikan parse tanggal indonesia dan urutan riwayat penjualan

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-/// <reference types="node" />
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: "./db/schema.ts",
-  out: "./db/migrations", // Kita pindahin folder migration dari Netlify ke folder db biar rapi
-  dialect: "postgresql",   // Kita ubah dialect-nya ke postgresql murni
+  schema: './db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: process.env.DATABASE_URL!,
   },
+  // TAMBAHKAN BARIS INI BIAR DRIZZLE GAK NGUTAK-NGATIK SUPABASE INTERNAL:
+  schemaFilter: ["public"], 
 });
